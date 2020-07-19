69535f95c6ca0d1b38207c3d253d9e5c40890aba
frederick tyteca <frederick@tyteca.net>
2020-07-19 21:56:18 +0200

    fix : forceSchema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (App::environment('production')) {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
